ed96d170346af2315477c68f95ee596bc7564635
kira2406 <kush.p030.24@gmail.com>
2025-01-07 02:41:48 -0500

    Added data validation component

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -9,12 +9,13 @@ defining common constant variable for training pipeline
 """
 TARGET_COLUMN = "Result"
 PIPELINE_NAME: str = "NetworkSecurity"
-ARTIFACT_DIR: str = "Artifacts"
+ARTIFACT_DIR: str = "artifacts"
 FILE_NAME: str = "phisingData.csv"
 
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 
 """
 Data ingestion related constants
@@ -25,3 +26,13 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_Store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+"""
+Data validation constants
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
